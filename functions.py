0e1cac0c6ab5a10f1fa535b2cf158af2707ed820
Luiz <valuiz1@gmail.com>
2021-11-27 18:59:23 +0000

    Erros de sintaxe SQL corrigidos Ainda necessidade de Except's

--- functions.py
+++ functions.py
@@ -1,23 +1,8 @@
 import random
 
-def insert(table, column1, columnv1, column2, columnv2, column3=None, columnv3=None, column4=None, columnv4=None):
-    insert_statement1 = f"INSERT INTO {table} ({column1}, {column2} "
-    if column3 is not None:
-        insert_statement1 += f",{column3} "
-    if column4 is not None:
-        insert_statement1 += f",{column4}"
-    insert_statement1 += f")"
-    value_statement = f" VALUE({columnv1}, {columnv2} "
-    if columnv3 is not None:
-        value_statement += f",{columnv3} "
-    if columnv4 is not None:
-        value_statement += f",{columnv4}"
-    value_statement +=f");"
-    return insert_statement1 + value_statement
-
 
 def insert_rpg():
-    insert_statement = f"INSERT INTO NPC (nome, profissão, nacionalidade, idade)"
+    insert_statement = f"INSERT INTO nergalia (nome, profissao, nacionalidade, idade)"
     sex_value = random.randint(1,2)
     if sex_value == 1:
         nomes_masc = organizador('nomes_masc.txt')
@@ -29,13 +14,13 @@ def insert_rpg():
         columnv1 = nomes_fem[random.randint(0,114)]
         profissoes_fem = organizador('profissoes_fem.txt')
         columnv2 = profissoes_fem[random.randint(0,13)]
-    nacionalidade = ["Ni'Kan", 'Proelia', 'Dannum', 'Zarbatus']
-    columnv3 = nacionalidade[random.randint(1,3)]
+    nacionalidade = ["NiKan", 'Proelia', 'Dannum', 'Zarbatus']
+    columnv3 = nacionalidade[random.randint(0,3)]
     if columnv2 in ['abençoado', 'abençoada']:
         columnv4 = random.randint(50,800)
     else:
         columnv4 = random.randint(16,110)
-    value_statement =f" VALUES ({columnv1}, {columnv2}, {columnv3}, {columnv4})\n"
+    value_statement =f" VALUES ('{columnv1}', '{columnv2}', '{columnv3}', {columnv4});\n"
     return insert_statement + value_statement
 
 
