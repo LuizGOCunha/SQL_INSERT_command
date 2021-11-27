8117bdce8a7bb975e0de9e7ce68054965ef51635
Luiz <valuiz1@gmail.com>
2021-11-27 18:10:25 +0000

    Criador de Comando SQL INSERT personalizado Simplificado para criar personagens do universo de Vidas Efêmeras

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
-print('Nome da tabela')
-table = input(">")
-print('Nome das colunas a serem utilizadas, separadas por virgula e espaço.')
-column_string = input(">")
-column_list = column_string.split(", ")
-if len(column_list) == 2:
-    column1, column2 = column_list
-elif len(column_list) == 3:
-    column1, column2, column3 = column_list
-elif len(column_list) == 4:
-    column1, column2, column3, column4 = column_list
-print('Valores das colunas, separadas por virgula e espaço.')
-value_string = input(">")
-value_list = value_string.split(", ")
-if len(value_list) == 2:
-    columnv1, columnv2 = value_list
-elif len(value_list) == 3:
-    columnv1, columnv2, columnv3 = value_list
-elif len(value_list) == 4:
-    columnv1, columnv2, columnv3, columnv4 = value_list
+from functions import *
+
+print('Bem-vindo ao criador de personagem, edição Nergália.')
+print('Insira abaixo a quantidade de Npcs que gostaria de criar.')
+rep = input('>')
+
+output = open('output.sql', 'w')
+quantidade = 'n' * int(rep)
+for n in quantidade:
+    output.write(insert_rpg())
+
+print('Output formado com sucesso')
+print('Verifique o arquivo de output para obter seu mock data')
 
 
-def insert(table, column1, columnv1, column2, columnv2, column3=None, columnv3=None, column4=None, columnv4=None):
-    insert_statement1 = f"INSERT INTO {table} ({column1}, {column2} "
-    if column3 is not None:
-        insert_statement1 += f",{column3} "
-    if column4 is not None:
-        insert_statement1 += f",{column4}"
-    insert_statement1 += f")"
-    value_statement = f" VALUE({columnv1}, {columnv2} "
-    if columnv3 is not None:
-        value_statement += f",{columnv3} "
-    if columnv4 is not None:
-        value_statement += f",{columnv4}"
-    value_statement +=f");"
-    return insert_statement1 + value_statement
 
-if len(column_list) == 2:
-    print(insert(table, column1, columnv1, column2, columnv2))
-elif len(column_list) == 3:
-    print(insert(table, column1, columnv1, column2, columnv2, column3, columnv3))
-elif len(column_list) == 4:
-    print(insert(table, column1, columnv1, column2, columnv2, column3, columnv3, column4, columnv4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
